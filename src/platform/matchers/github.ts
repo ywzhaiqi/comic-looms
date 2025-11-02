822cc5047bfcb789aa60cd1801bab6fc454ed6b6
MapoMagpie <zsyjk@live.cn>
2025-11-02 20:27:37 +0800

    chore: github;

--- src/platform/matchers/github.ts
+++ src/platform/matchers/github.ts
@@ -1,5 +1,4 @@
 import ImageNode from "../../img-node";
-import { Chapter } from "../../page-fetcher";
 import { ADAPTER } from "../adapt";
 import { BaseMatcher, OriginMeta, Result } from "../platform";
 
@@ -9,22 +8,18 @@ type GHImage = {
 }
 
 class GithubMatcher extends BaseMatcher<GHImage[]> {
-  async *fetchChapters(): AsyncGenerator<Chapter[]> {
-    yield [new Chapter(1, "README", window.location.href)];
-  }
-  async *fetchPagesSource(chapter: Chapter): AsyncGenerator<Result<GHImage[]>> {
-    if (chapter.title === "README") {
-      const images = Array.from(document.querySelectorAll<HTMLImageElement>("article.markdown-body img"));
-      const ret = images.map((img, i) => {
-        const src = img.src;
-        if (!src) return undefined;
-        const name = src.split("/").pop() ?? (i + 1) + ".jpg";
-        return { name, url: src };
-      }).filter(img => img !== undefined);
-      yield Result.ok(ret);
-    } else {
-      yield Result.ok([]);
-    }
+  // async *fetchChapters(): AsyncGenerator<Chapter[]> {
+  //   yield [new Chapter(1, "README", window.location.href)];
+  // }
+  async *fetchPagesSource(): AsyncGenerator<Result<GHImage[]>> {
+    const images = Array.from(document.querySelectorAll<HTMLImageElement>(".markdown-body img"));
+    const ret = images.map((img, i) => {
+      const src = img.src;
+      if (!src) return undefined;
+      const name = src.split("/").pop() ?? (i + 1) + ".jpg";
+      return { name, url: src };
+    }).filter(img => img !== undefined);
+    yield Result.ok(ret);
   }
   async parseImgNodes(images: GHImage[]): Promise<ImageNode[]> {
     return images.map((img) => new ImageNode("", img.url, img.name, undefined, img.url));
@@ -39,7 +34,8 @@ ADAPTER.addSetup({
   name: "Github",
   workURLs: [
     /github.com\/MapoMagpie\/comic-looms$/,
-    // /github.com\/[\w_.-]+\/[\w_.-]+\/?$/,
+    // /github.com\/[\w_.-]+\/[\w_.-]+(\/.*\.md|)?$/,
+    // /github.com\/[\w_.-]+\/[\w_.-]+\/wiki/,
   ],
   constructor: () => new GithubMatcher(),
 });
